264b47d9aea36e2e3ec6249b6aebe6f08f6d63d3
MichaelTan <thr205@hotmail.com>
2019-11-28 00:37:18 +0800

    Initial Commit

---
+++ producer&consumer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  producer&consumer
+//
+//  Created by Michael Tan on 2019/11/28.
+//  Copyright © 2019 MichaelTan9999. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
